3ff7fa42a94edce88d3bbd4c4287d3d0c28defa1
Tony Fischetti <tony.fischetti@gmail.com>
2014-01-07 21:32:13 -0500

    revised project description

--- setup.py
+++ setup.py
@@ -47,16 +47,15 @@ setup(name=constants.NAME,
       url=constants.URL,
       license='MIT',
       long_description="""
-      Sake is a way to easily design, share, build, and visualize
-      workflows with intricate interdependencies. Sake is self-documenting
-      because the instructions for building a project also serve as the
-      documentation of the project's workflow. The first time it's run,
-      sake will build the whole project in an order that automatically
-      satisfies all dependencies. For all subsequent runs, sake will only
-      rebuild the parts of the project that depend on changed files. This
-      cuts down on unnecessary re-building and let's you concentrate on
-      your work rather than memorizing the order in which commands have to
-      be run.
+      Sake is a way to easily design, share, build, and visualize workflows
+      with intricate interdependencies. Sake is self-documenting because the
+      instructions for building a project also serve as the documentation of
+      the project's workflow. The first time it's run, sake will build all
+      of the components of a project in an order that automatically satisfies
+      all dependencies. For all subsequent runs, sake will only rebuild the
+      parts of the project that depend on changed files. This cuts down on
+      unnecessary re-building and lets the user concentrate on their work
+      rather than memorizing the order in which commands have to be run.
 
       For more information and documentation, check out this project's
       website: http://tonyfischetti.github.io/sake/

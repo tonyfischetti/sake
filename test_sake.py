b5cb1a1a375495d2bd4d2fcb19015dbb2cfaa33e
Tony Fischetti <tony.fischetti@gmail.com>
2014-08-25 14:57:31 -0400

    more unittesting

--- test_sake.py
+++ test_sake.py
@@ -12,14 +12,12 @@ class TestActsFunction(unittest.TestCase):
 
     def setUp(self):
         # for mock sakefile 1
-        lines = ["---", "#!ask=shyness is nice",
-                 "  ", "#!rusholme = at the last night      ",
+        lines = ["---", "#!ask=$hyness is nice",
+                 "  ", "#!rΩsholme = at the last night      ",
                  "panic #! streets= london, burmingham",
                  " #! asleep =sing me to sleep",
                  "..."]
-        self.mock_sakefile_1 = "\n".join(lines)
-        print(self.mock_sakefile_1)
-        print(acts.gather_macros(self.mock_sakefile_1))
+        self.mock_sakefile_for_macros = "\n".join(lines)
 
 
     def test_escp(self):
@@ -31,6 +29,30 @@ class TestActsFunction(unittest.TestCase):
         self.assertEqual("\"rusholme ruffians\"", acts.escp(has_one_space))
         self.assertEqual("\"shakespeare's  sister\"", acts.escp(has_two_cons_spaces))
         self.assertEqual("\" well i wønder\"", acts.escp(has_more_spaces_and_unicode))
+
+    def test_gather_macros(self):
+        self.assertEqual(acts.gather_macros(self.mock_sakefile_for_macros),
+                         {"ask": "$hyness is nice",
+                          "rΩsholme": "at the last night      "})
+        self.assertFalse(acts.gather_macros("---\nhandsome devil\n..."))
+
+    def test_expand_macros(self):
+        # ATTENTION:
+        #   there is a bug in python's template substitution that
+        #   prevents certain unicode-heavy strings from being replaced
+        #   so 'rΩsholme' won't be subsituted, but it should be
+        temp = ["---", "#!ask=shyness is nice", "$ask me, ask me, $ask me",
+                "there are ruffians in $rusholme ($rΩsholme) and they steal $$",
+                "$askme, ${ask}me", "..."]
+        temp = "\n".join(temp)
+        solution = ["---", "#!ask=shyness is nice", "$hyness is nice me, ask me, $hyness is nice me",
+                    "there are ruffians in $rusholme ($rΩsholme) and they steal $",
+                    "$askme, $hyness is niceme", "..."]
+        solution = "\n".join(solution)
+        self.assertEqual(acts.expand_macros(temp,
+                                 acts.gather_macros(self.mock_sakefile_for_macros)),
+                         solution)
+
 
 
 if __name__ == '__main__':

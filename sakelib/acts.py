40b03152e86655441a746a85f7d562f20ca4ed10
Tony Fischetti <tony.fischetti@gmail.com>
2014-11-25 12:48:08 -0500

    added test for "acts.clean_path"

--- sakelib/acts.py
+++ sakelib/acts.py
@@ -56,13 +56,24 @@ if sys.version_info[0] < 3:
     import codecs
     open = codecs.open
 
-def clean_path(a_path):
+def clean_path(a_path, force_os=None, force_start=None):
     """
     This function is used to normalize the path (of an output or
     dependency) and also provide the path in relative form. It is
     relative to the current working directory
     """
-    return os.path.relpath(os.path.normpath(a_path))
+    if not force_start:
+        force_start = os.curdir()
+    if force_os == "windows":
+        import ntpath
+        return ntpath.relpath(ntpath.normpath(a_path),
+                             start=force_start)
+    if force_os == "posix":
+        import posixpath
+        return posixpath.relpath(posixpath.normpath(a_path),
+                                start=force_start)
+    return os.path.relpath(os.path.normpath(a_path),
+                          start=force_start)
 
 
 def escp(target_name):
--- test_sake.py
+++ test_sake.py
@@ -3,6 +3,8 @@
 
 import unittest
 from sakelib import acts
+import ntpath
+import posixpath
 
 
 # test UNICODE!!!
@@ -19,6 +21,19 @@ class TestActsFunction(unittest.TestCase):
                  "..."]
         self.mock_sakefile_for_macros = "\n".join(lines)
 
+    def test_clean_path(self):
+        unixpath1 = "/home/krsone/Pictures/../Desktop/"
+        unixpath2 = "/home/krsone/Pictures/./me.jpg"
+        windowspath1 = "C:/User/scottlarack/Pictures/../Desktop/"
+        windowspath2 = "C:\\User/scottlarack/Pictures/./me.png"
+        self.assertEqual(acts.clean_path(unixpath2,
+                                         force_os="posix",
+                                         force_start=posixpath.normpath(unixpath1)),
+                         "../Pictures/me.jpg")
+        self.assertEqual(acts.clean_path(windowspath2,
+                                         force_os="windows",
+                                         force_start=ntpath.normpath(windowspath1)),
+                         "..\\Pictures\\me.png")
 
     def test_escp(self):
         has_no_space = "ask"
@@ -35,10 +50,10 @@ class TestActsFunction(unittest.TestCase):
                          {"ask": "$hyness is nice",
                           "rΩsholme": "at the last night      "})
         self.assertRaises(acts.InvalidMacroError,
-                          acts.gather_macros, 
+                          acts.gather_macros,
                           "---\n#!f4I7 ur3=this should fail\n...")
         self.assertRaises(acts.InvalidMacroError,
-                          acts.gather_macros, 
+                          acts.gather_macros,
                           "---\n#!===...")
         self.assertFalse(acts.gather_macros("---\nhandsome devil\n..."))
 

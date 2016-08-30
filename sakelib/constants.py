b9ce50d13f581e5633cec26f2ac498b10cd70ac9
Tony Fischetti <tony.fischetti@gmail.com>
2016-08-30 12:16:12 -0400

    changes the process name if it can

--- sakelib/constants.py
+++ sakelib/constants.py
@@ -39,7 +39,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 # Version number
-VERSION = "0.9.7"
+VERSION = "0.9.71"
 
 # Name of application
 NAME = 'master-sake'

941bc4d5eafe664a6e7ffa3523acc230273c164b
Tony Fischetti <tony.fischetti@gmail.com>
2016-08-30 15:12:16 -0400

    updated CHANGES and version number

--- sakelib/constants.py
+++ sakelib/constants.py
@@ -39,7 +39,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 # Version number
-VERSION = "0.9.71"
+VERSION = "0.9.8"
 
 # Name of application
 NAME = 'master-sake'
